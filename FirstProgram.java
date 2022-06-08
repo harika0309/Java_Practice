4eb027c0e70f87af9bfa17e4a408eec2d0b8024d
Harika Sabbineni <Harika@Harin-mac.local>
2022-06-08 18:51:38 -0400

    first program

---
+++ FirstProgram.java
@@ -0,0 +1,32 @@
+class FirstProgram {
+	
+	public static void main(String[] args) {
+		int X =10;
+		float Y = 20.1f;
+		long Z = 3000;
+		double d = 4000000;
+		String myName = "Harika";
+		boolean isTrue = false;
+		char mySymbol = 'H';
+	
+		System.out.println(X);
+		System.out.println(Y);
+		System.out.println(Z);
+		System.out.println(d);
+		System.out.println(myName);
+		System.out.println(isTrue);
+		System.out.println(mySymbol);
+
+		//call updateDatabase
+		//call deleteDatabase
+	}
+
+	private void updateDatabase(String userName){
+		//code to save user 
+	}
+
+	private void deleteDatabase(String userName){
+		//delete to user
+	}
+
+}
